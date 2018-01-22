eda26898b4448fd01eec2af1bbadde14810bc8e0
MostVal <gyemalin@gmail.com>
2018-01-21 21:05:44 -0600

    Upload the lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,17 +2,23 @@ import boto3
 import StringIO
 import zipfile
 import mimetypes
-s3 = boto3.resource('s3')
-
-portfolio_bucket = s3.Bucket('portfolio.sky-vaults.com')
-build_bucket = s3.Bucket('portfoliobuild.sky-vaults.com')
 
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-        for mm in myzip.namelist():
-            obj = myzip.open(mm)
-            portfolio_bucket.upload_fileobj(obj,mm, ExtraArgs={'ContentType': mimetypes.guess_type(mm)[0]})
-            portfolio_bucket.Object(mm).Acl().put(ACL='public-read')
+    portfolio_bucket = s3.Bucket('portfolio.sky-vaults.com')
+    build_bucket = s3.Bucket('portfoliobuild.sky-vaults.com')
+
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+            for mm in myzip.namelist():
+                obj = myzip.open(mm)
+                portfolio_bucket.upload_fileobj(obj,mm, ExtraArgs={'ContentType': mimetypes.guess_type(mm)[0]})
+                portfolio_bucket.Object(mm).Acl().put(ACL='public-read')
+
+    print "job Done"
+    return "Hello World"
